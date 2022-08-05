fed8445161557bc31f8ac7d763f69923a19b914b
TetianaMudriak <tetiana.mudriak48@gmail.com>
2022-08-06 00:20:15 +0300

    Added 3 task

--- Iterators_generators.py
+++ Iterators_generators.py
@@ -30,13 +30,45 @@ def in_range(iterable: RangeCreating):
     print(list_)
 
 
+class Iterator:
+    def __init__(self, iterable):
+        self._iterable = iterable
+        self._index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._index < len(self._iterable):
+            self._index += 1
+            return self._iterable[self._index]
+        raise StopIteration
+
+
+class Iterable:
+    def __init__(self, data):
+        self._data = data
+
+    def __iter__(self):
+        return Iterator(self._data)
+
+    def __getitem__(self, item):
+        return self._data[item]
+
+
 def main():
     # Task 1
     with_index(['a', 'b', 'c'])
 
     # Task 2
     # range1 = RangeCreating(1, 5)
-    in_range(RangeCreating(1, 6))
+    in_range(RangeCreating(1, 8, 2))
+
+    # Task 3
+    list1 = [1, 2, 3]
+    print(len(list1))
+    iter1 = Iterable(list1)
+    print(iter1[0])
 
 
 if __name__ == '__main__':
